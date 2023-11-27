16b196b86889bac707cd3061e7c0950756e0a1bb
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-11-27 14:46:46 +0100

    added a basic check to make sure the typed text is the same as the overlay

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+const typingBoardElement = document.querySelector('#typingBoard');
+// const overlayElement = document.querySelector('#typingOverlay');
+const overlayTextElement = document.querySelector('#overlayText')
+
+let currentIndex = 0
+
 document.addEventListener('keydown', function (event){
-    const typingBoardElement = document.querySelector('.typingBoard');
-    typingBoardElement.textContent += event.key
+    const overlayText = overlayTextElement.textContent;
+
+    if (event.key === overlayText.charAt(currentIndex)){
+        typingBoardElement.textContent += event.key
+        currentIndex++;
+    }else{
+        console.log('Nah ahh')
+    }
+
+    // const typedCharacter = typingBoardElement.textContent.charAt(currentIndex);
+
+    // if (typedCharacter === overlayText.charAt(currentIndex)) {
+    //     // typed char is correct
+    //     currentIndex++;
+
+    // }
+
 })
 
